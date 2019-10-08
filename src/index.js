e3db708c7d654e581c26c0a4e6ed0e6f65e779f4
Siarhei Prymakovich <prima.psa@gmail.com>
2019-10-09 01:14:27 +0300

    feat: implement part of calculator fnc

--- src/index.js
+++ src/index.js
@@ -5,7 +5,73 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let separated = [];
+    let separated2 = [];
+    let symbol = '';
+    let operation;
+    let temp = [];
+    let current = '';
+    
+    let operands = ['/', '*', '-', '+'];
+    
+        expr = expr.split('');
+        for(let i=0; i < expr.length; i++){      
+          if(separated[i] != '' || separated[i] != ' ' ){
+            if(operands.indexOf(expr[i])!=-1){
+              separated.push(parseFloat(symbol));
+              separated.push(expr[i]); 
+              symbol= '';
+            }
+            else{
+              symbol+=expr[i];
+            }
+        } 
+        }
+    if(symbol!='')separated.push(parseFloat(symbol));  
+    
+    
+    
+    for (let i = 0; i < operands.length; i++){
+    
+      for(let j = 0; j <= separated.length; j++){     
+          
+        if(operands[i] == separated[j]){
+          current = operands[i];        
+        } 
+        else if(current!=''){
+          switch(current){
+            case '/':
+              temp[temp.length-1] = temp[temp.length-1] / separated[j];
+              if(separated[j] == 0) throw("TypeError: Division by zero.");
+              break;
+              case '*':
+              temp[temp.length-1] = temp[temp.length-1] * separated[j];
+              break;
+               case '+':
+              temp[temp.length-1] = temp[temp.length-1] + separated[j];
+              break;
+               case '-':
+              temp[temp.length-1] = temp[temp.length-1] - separated[j];
+              break;
+          }
+          current ='';
+        }
+          else{
+            if(separated[j])
+            temp.push(separated[j]); 
+          }
+             
+       
+           
+        } 
+         separated = temp;
+        temp = [];
+     
+      } 
+    return separated[0];
 }
+
+
 
 module.exports = {
     expressionCalculator
