5b9945e4397c95e795695dbd88a251c0e2e92470
Siarhei Prymakovich <prima.psa@gmail.com>
2019-10-10 17:37:34 +0300

    refactor: delete white-spacing, tab etc

--- src/index.js
+++ src/index.js
@@ -3,117 +3,39 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
-    // let separated = [];
-    // let separated2 = [];
-    // let symbol = '';
-    // let operation;
-    // let temp = [];
-    // let current = '';
-    
-    // let operands = ['/', '*', '-', '+'];
-    
-    //     expr = expr.split('');
-    //     for(let i=0; i < expr.length; i++){      
-    //       if(separated[i] != '' || separated[i] != ' ' ){
-    //         if(operands.indexOf(expr[i])!=-1){
-    //           separated.push(parseFloat(symbol));
-    //           separated.push(expr[i]); 
-    //           symbol= '';
-    //         }
-    //         else{
-    //           symbol+=expr[i];
-    //         }
-    //     } 
-    //     }
-    // if(symbol!='')separated.push(parseFloat(symbol));  
-    
-    
-    
-    // for (let i = 0; i < operands.length; i++){
-    
-    //   for(let j = 0; j <= separated.length; j++){     
-          
-    //     if(operands[i] == separated[j]){
-    //       current = operands[i];        
-    //     } 
-    //     else if(current!=''){
-    //       switch(current){
-    //         case '/':
-    //           temp[temp.length-1] = temp[temp.length-1] / separated[j];
-    //           if(separated[j] == 0) throw("TypeError: Division by zero.");
-    //           break;
-    //           case '*':
-    //           temp[temp.length-1] = temp[temp.length-1] * separated[j];
-    //           break;
-    //            case '+':
-    //           temp[temp.length-1] = temp[temp.length-1] + separated[j];
-    //           break;
-    //            case '-':
-    //           temp[temp.length-1] = temp[temp.length-1] - separated[j];
-    //           break;
-    //       }
-    //       current ='';
-    //     }
-    //       else{
-    //         if(separated[j])
-    //         temp.push(separated[j]); 
-    //       }
-             
-       
-           
-    //     } 
-    //      separated = temp;
-    //     temp = [];
-     
-    //   } 
-    // return separated[0];
+function expressionCalculator(expr) {  
 
-
+const operands = ['/', '*', '-', '+', '(', ')'];
 
     function splitExpression (expr){
-      let separated = [];
-      let symbol = '';
-       let operands = ['/', '*', '-', '+', '(', ')'];
+      let separated = [],
+          symbol = '';    
       
-       expr = expr.split('');
-            for(let i=0; i < expr.length; i++){         
-              
-              if(separated[i] !== '' || separated[i] !== ' ' ){
-               // console.log('current: '+ expr[i] + 'prev: '+ expr[i-1]+'position: '+separated[separated.length-2]);
-                
-               if((operands.indexOf(expr[i])===-1) || i==0 ||
-               ((expr[i]==='-') && (operands.indexOf(expr[i-1])!=-1) )){
-                symbol+=expr[i];
-               
-               }
-            
-               else{
-                if(parseFloat(symbol) ){
-                 separated.push(parseFloat(symbol));
-               
-                  } 
-              
-              separated.push(expr[i]); 
-              symbol= '';
-               }
-               // console.log(+separated);
-
-              
+          expr = expr.split('');
+            for(let i=0; i < expr.length; i++){                 
+              if(separated[i] !== '' || separated[i] !== ' ' ){               
+                if((operands.indexOf(expr[i])===-1) || (i==0 && expr[i]!='(') ||
+                ((expr[i]==='-') && (operands.indexOf(expr[i-1])!=-1) )){
+                  symbol+=expr[i];               
+                }            
+                else{
+                if(parseFloat(symbol) || parseFloat(symbol)==0){
+                 separated.push(parseFloat(symbol));               
+                }               
+                separated.push(expr[i]); 
+                symbol= '';
+               }           
             }
           }
-        if(symbol!='')separated.push(parseFloat(symbol)); 
-     // console.log('separated '+separated);
-      return separated; 
-  }  
-  
+        if(symbol!='')separated.push(parseFloat(symbol));      
+     
+       return separated; 
+  }    
 
 
 function calc(expression){
- let temp = [];
-  let current = ''; 
-  let operands = ['/', '*', '-', '+', '(', ')'];
+ let temp = [],
+     current = '' ;  
   
 for (let i = 0; i < operands.length; i++){    
       for(let j = 0; j <= expression.length; j++){            
@@ -139,7 +61,7 @@ for (let i = 0; i < operands.length; i++){
           current ='';
         }
           else{
-            if(expression[j])
+            if(expression[j] || expression[j]==0 )
             temp.push(expression[j]); 
           }              
         } 
@@ -151,108 +73,79 @@ for (let i = 0; i < operands.length; i++){
 
 function splitBrackets (separated){
   let bracket =[], 
-      bracket2 =[],
+      bracketFinal =[],
       woBracket = '';
-  for(let j = 0; j < separated.length; j++ ){ 
-  if( separated[j] == '(' || separated[j] == ')'){
-     bracket.push(woBracket);
-     bracket.push(separated[j]);
-     woBracket = '';
-  }   
-  
-  else {
-     woBracket  += separated[j];
-  }  
-}
-if(woBracket!='') bracket.push(woBracket);
-//console.log(bracket);
-for(let k =0; k<bracket.length; k++){
-      if(bracket[k]!=''){
-        bracket2.push(bracket[k]);
-      }
-}
-//console.log(bracket2);
-  return bracket2;
+
+        for(let j = 0; j < separated.length; j++ ){ 
+          if( separated[j] == '(' || separated[j] == ')'){
+            bracket.push(woBracket);
+            bracket.push(separated[j]);
+            woBracket = '';
+          }           
+          else {
+            woBracket  += separated[j];
+          }  
+          }
+          if(woBracket!='') bracket.push(woBracket);
+
+            for(let k =0; k<bracket.length; k++){
+                  if(bracket[k]!=''){
+                    bracketFinal.push(bracket[k]);
+            }
+          }
+
+  return bracketFinal;
 }
 
-function bracketSum(bracket2) {
- let currentBracket = '';
-let tempBracket =[];
-  let subResult =[];
-  let subString = '';
-  let dd = 0;
-  let leftBr = 0, rightBr =0;
-  let subExptr = []; let cnt = 0;
-  //console.log(bracket2);
+function bracketSum(bracketFinal) {
+ let currentBracket = '',
+       tempBracket =[],
+       subResult =[],
+       subString = '',
+       dd = 0,
+       leftBr = 0, rightBr =0,
+       subExptr = [], 
+       cnt = 0;
+  
 for(let cc = 0; cc < 1; cc++){
-for(let l=0; l < bracket2.length; l++){
-  
-  if(bracket2[l] == '('){
-    leftBr++;
-  }
-  if(bracket2[l] == ')'){
-    rightBr++;
-    if(leftBr > 0 && rightBr==1 ){
+    for(let l=0; l < bracketFinal.length; l++){
       
-      for(let v = tempBracket.length-1; v >= 0; v--){
-        //console.log('br: '+bracket2[v]);
-        if(tempBracket[v] == '('){
-          subExptr.reverse()
-          let strrr = subExptr.join('');
-         // console.log('send: '+strrr);
-          
-          subResult = calc(splitExpression(strrr));
-        //  console.log('subresult: '+subResult);
-          tempBracket.splice(tempBracket.length-cnt-1, cnt+1);
-          //console.log(tempBracket);
-          tempBracket.push(subResult);
-         // console.log(tempBracket);
-          cnt = 0; --leftBr; --rightBr; subExptr=[];
-          break;
-         
-        }
-        subExptr.push(tempBracket[v]);
-        cnt++;
+      if(bracketFinal[l] == '('){
+        leftBr++;
       }
-      continue
-    }
-  } 
-  //console.log('push');
-  
-  tempBracket.push(bracket2[l]); 
-  //console.log(tempBracket);
-    
-    
-
-  
-} 
-  
-  bracket2 = tempBracket;
- 
-   tempBracket = [];
-   if(leftBr > 0 || rightBr > 0){
+      if(bracketFinal[l] == ')'){
+        rightBr++;
+        if(leftBr > 0 && rightBr==1 ){          
+          for(let v = tempBracket.length-1; v >= 0; v--){           
+            if(tempBracket[v] == '('){
+              subExptr.reverse()
+              subString = subExptr.join('');             
+              subResult = calc(splitExpression(subString));            
+              tempBracket.splice(tempBracket.length-cnt-1, cnt+1);              
+              tempBracket.push(subResult);           
+              cnt = 0; --leftBr; --rightBr; subExptr=[], subString = '';
+              break;            
+            }
+            subExptr.push(tempBracket[v]);
+            cnt++;
+          }
+          continue
+        }
+      }       
+      tempBracket.push(bracketFinal[l]);      
+    }   
+  bracketFinal = tempBracket;
+  tempBracket = [];
+  if(leftBr > 0 || rightBr > 0){
     throw("ExpressionError: Brackets must be paired");
    }
-} //console.log(bracket2);
-  let brrkds = bracket2.join('');
-   let res=calc(splitExpression(brrkds));
-   return res;
+} 
+ 
+   return calc(splitExpression(bracketFinal.join('')));
 
 }
 
-//console.log(expr)
-let arrcs = splitExpression(expr);
-//console.log(arrcs);
-let summ = splitBrackets(arrcs);
-//console.log(summ);
-let blbb = bracketSum(summ);
-//console.log(blbb);
-return blbb;
-//console.log(blbb);
-
-
-
-
+return(bracketSum(splitBrackets(splitExpression(expr))));
  }
 
 module.exports = {
